57bca0e8315d73aa382b725defe6780e0c0f604b
Aspaturyan <hayk.zaqarich@gmail.com>
2022-02-09 16:20:42 +0400

    update_2

--- final_project/src/components/navBar/index.js
+++ final_project/src/components/navBar/index.js
@@ -8,18 +8,18 @@ const NavBar = () => {
                 <NavLink to= {'/telephone'} href='+374 911119'> +374 911119 </NavLink>
             </div>
             <div className="navigation">
-                <NavLink to={'/books'}> Գրքեր </NavLink>
-                <NavLink to={'/about'}> Մեր մասին </NavLink>
-                <NavLink to={'/news'}> Նորություններ </NavLink>
-                <NavLink to={'/blog'}> Բլոգ </NavLink>
+                <NavLink  to={'/books'}> Գրքեր </NavLink>
+                <NavLink  to={'/about'}> Մեր մասին </NavLink>
+                <NavLink  to={'/news'}> Նորություններ </NavLink>
+                <NavLink  to={'/blog'}> Բլոգ </NavLink>
             </div>
             <div className=' selected'>
-                <select name="change" id="change">
+                <select className='select' name="change" id="change">
                     <option value = 'AMD' > AMD </option>
                     <option value = 'RUB' > RUB </option>
                     <option value = 'USD' > USD </option>
                 </select>
-                <select name="language" id="language">
+                <select className='select' name="language" id="language">
                     <option value = 'Arm' > Հայ</option>
                     <option value = 'Rus' > Рус</option>
                     <option value = 'Eng' > Eng </option>
--- final_project/src/components/searchBar/index.js
+++ final_project/src/components/searchBar/index.js
@@ -9,12 +9,12 @@ const SearchBar = () => {
                         <NavLink to={'/'}><img className="logo" src="/icons/2815428.png" alt="logo"/></NavLink>
                     </div>
                     <div className="searchings">
-                        <input type={'text'}></input>
-                        <button><img className="icon_search" src="/icons/156545.png" alt="serach"/></button>
+                        <input  className="input_button" type={'text'} placeholder={"Որոնել"}></input>
+                        <button className="input_button" ><img className="icon_search" src="/icons/156545.png" alt="serach"/></button>
                     </div>
                     <div className="action">
                         <div><img className="icon_user" src = "/icons/958588.png" alt="icon"/></div>
-                        <div><NavLink  className="action_btn" to={'/registration'}> Մուտք </NavLink></div>
+                        <div><NavLink  className="action_btn icon_user" to={'/registration'}> Մուտք </NavLink></div>
                     </div>
                     <div className=" action preferred">
                         <div><img className="icon_love" src = "/icons/2623043.png" alt="icon"/></div>
