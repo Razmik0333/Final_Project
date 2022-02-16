08da515cf086a26a7507ed37003659964443fd6c
Aspaturyan <hayk.zaqarich@gmail.com>
2022-02-16 20:07:56 +0400

    header_file_apdate_2

--- final_project/src/components/Header/headerCompontnts/SearchBar/SearchBar.jsx
+++ final_project/src/components/Header/headerCompontnts/SearchBar/SearchBar.jsx
@@ -17,24 +17,27 @@ const SearchBar = () => {
                             <img className="icon_search" src="/icons/156545.png" alt="serach"/>
                         </button>
                     </div>
-                    <div className="action_user">
-                        <NavLink className="action_btn" to={'/registration'}>
-                            <img className="icon_user" src = "/icons/3329703.png" alt="icon"/> 
-                            <span> Մուտք </span>
-                        </NavLink>
+                    <div className="action">
+                        <div className="action_user">
+                            <NavLink className="action_btn" to={'/registration'}>
+                                <img className="icon_user" src = "/icons/3329703.png" alt="icon"/> 
+                                <span> Մուտք </span>
+                            </NavLink>
+                        </div>
+                        <div className="action_preferred">
+                            <NavLink className="action_btn" to={'/Preferred'}>
+                                <img className="icon_love" src = "/icons/9358186.png" alt="icon"/> 
+                                <span> Նախընտրելի </span> 
+                            </NavLink>
+                        </div>
+                        <div className="action_basket">
+                            <NavLink className="action_btn" to={'/basket'}> 
+                                <img className="icon_love" src = "/icons/9349824.png" alt="icon"/>
+                                <span> Զամբյուղ </span> 
+                            </NavLink>
+                        </div> 
                     </div>
-                    <div className="action_preferred">
-                        <NavLink className="action_btn" to={'/Preferred'}>
-                            <img className="icon_love" src = "/icons/9358186.png" alt="icon"/> 
-                            <span> Նախընտրելի </span> 
-                        </NavLink>
-                    </div>
-                    <div className="action_basket">
-                        <NavLink className="action_btn" to={'/basket'}> 
-                            <img className="icon_love" src = "/icons/9349824.png" alt="icon"/>
-                            <span> Զամբյուղ </span> 
-                        </NavLink>
-                    </div> 
+                    
                 </div>
             </div>
         </>
