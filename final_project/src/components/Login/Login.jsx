88eb19c7daf0da092bff879a00f3d76721dbe405
unknown <you@example.com>
2022-02-25 21:45:27 +0300

    register  commit

--- final_project/src/components/Login/Login.jsx
+++ final_project/src/components/Login/Login.jsx
@@ -1,82 +1,87 @@
 import { useState } from "react";
-import {NavLink} from "react-router-dom";
 import './Register.css';
-
- 
+import {NavLink}from "react-router-dom";
 
 function Login(){
     const [email, setEmail]=useState("");
     const [password, setPassword]=useState("");
+    const [allEntry,setAllEntry]=useState([]);
     const [submite,setSubmite]=useState("");
     const [error, setError]=useState("")
-    
-    function handlePassword(evt){
-        setPassword(evt.target.value);
-        setSubmite(false)
-
-    }
-    function handleEmail(event){
-      setEmail(event.target.value);
-      setSubmite(false)
-    }
-
-    function handleSubmit(evt){
-       evt.preventDefault();
-       if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)|| email==="" || password==="" ){
-           setError(true)
-       }
-       else{
-           setSubmite(true);
-           setError(false);
-       }
-    }
-
-
-
    
+//     function submiteForm(e){
+//     e.preventDefault()
+   
+//    const newEntry={email:email,password:password};
+//    setAllEntry([...allEntry, newEntry]);
+//    console.log(allEntry);
 
-    function errorMessage(){
-      return(
-          <div className="error" style={{
-            display: error ? '' : 'none',
-          }}>
-              <span className="error_mesiges">Խնդրում ենք ճիշտ մուտքագրել բոլոր դաշտերը</span>
+//     }
+    function handleSubmit(evt){
+        evt.preventDefault();
+     
+        if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)|| email==="" || password==="" ){
+            setError(true)
+        }
+        else{
+            setSubmite(true);
+            setError(false);
+        }
+
+        const newEntry={email:email,password:password};
+        setAllEntry([...allEntry, newEntry]);
+        console.log(allEntry);
+     }
+
+     const successMessage = () => {
+        return (
+          <div
+            className="success"
+            style={{
+              display:submite ? '' : 'none',
+            }}>
+            <span className="error_mesiges">շնորհավորում ենք  դուք  մուտք գործեցիք!!</span>
           </div>
-      )
-    }
-    
+        );
+      };
+  
+  
+     
+  
+      function errorMessage(){
+        return(
+            <div className="error" style={{
+              display: error ? '' : 'none',
+            }}>
+                <span >Խնդրում ենք ճիշտ մուտքագրել բոլոր դաշտերը</span>
+            </div>
+        )
+      }
+      
+  
+
+
 
 
     return(
+          <div className="login">
+              
+            <form className="login_form" >
+                 <h2>ԳՐԱՆՑՎԱԾ ՀԱՃԱԽՈՐԴ</h2>
+                 <div className="errorMessages">
+                {errorMessage()}
+                {successMessage()}
+              </div>
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} name="email" placeholder="էլ․ հասցե" autoComplete="off" type="text"/>
+                <input onChange={(e)=>setPassword(e.target.value)} value={password} name="password" placeholder="Գաղտնաբառ" autoComplete="off" type="password"/>
+                <button onClick={handleSubmit}  className="login_btn" type="submit">Մուտք</button>
+                <span>Եթե չունեք հաշիվ, կարող եք  <NavLink className='navLink777' to='/register'>Գրանցվել </NavLink></span>
+                
 
-        
+             </form>
+            
 
-        <div className="login">
-
-      <div>
-        <h2>ԳՐԱՆՑՎԱԾ ՀԱՃԱԽՈՐԴ</h2>
-      </div>
- 
-      <div className="errorMessages">
-        {errorMessage()}
-      </div>
-
-
-
-
-          <form className="login_form">
-              <input value={email} onChange={handleEmail} placeholder="էլ, հասցե" className="log_inp" type="text"/>
-              <br/>
-              <input value={password} onChange={handlePassword} placeholder="Գաղտնաբառ" className="log_inp" type="password"/>
-              <br/>
-
-              <button onClick={handleSubmit}  className="login_btn" type="Submit">Մուտք</button>
-              <span>Եթե չունեք հաշիվ, կարող եք  <NavLink className='navLink777' to='/register'>Գրանցվել </NavLink></span> 
-
-
-          </form>
         </div>
     )
 }
-
 export default Login
--- final_project/src/components/Register/Register.jsx
+++ final_project/src/components/Register/Register.jsx
@@ -1,107 +1,118 @@
-import { useState } from "react"
-import {NavLink} from "react-router-dom"
 import './Register.css';
-
+import { useState } from "react";
+import {NavLink} from "react-router-dom"
 function Register(){
-    const [name,setName]=useState("");
-    const [surname,setSurname]=useState("");
+    const [name, setName]=useState("");
+    const [surname, setSurname]=useState("");
     const [age, setAge]=useState("");
     const [email, setEmail]=useState("");
     const [password, setPassword]=useState("");
+    const [allEntry,setAllEntry]=useState([]);
     const [submite,setSubmite]=useState("");
-    const [error, setError]=useState("");
-
-
-  function handleName(evt){
-    setName(evt.target.value);
-      setSubmite(false)
-
-    }
-
-    function handleSurname(evt){
-        setSurname(evt.target.value);
-        setSubmite(false)
-
-   }
+    const [error, setError]=useState("")
    
+
+    function handleName(evt){
+        setName(evt.target.value);
+          setSubmite(false)
     
-    function handleAge(evt){
-        setAge(evt.target.value);
-        setSubmite(false)
+        }
+    
+        function handleSurname(evt){
+            setSurname(evt.target.value);
+            setSubmite(false)
+    
+       }
+       
+        
+        function handleAge(evt){
+            setAge(evt.target.value);
+            setSubmite(false)
+    
+        }
+    
+        function handleEmail(evt){
+          setEmail(evt.target.value);
+          setSubmite(false)
+    
+      }
+        function handlePassword(evt){
+            setPassword(evt.target.value)
+            setSubmite(false)
+    
+        }
 
-    }
 
-    function handleEmail(evt){
-      setEmail(evt.target.value);
-      setSubmite(false)
-
-  }
-    function handlePassword(evt){
-        setPassword(evt.target.value)
-        setSubmite(false)
-
-    }
-
-    function handleSubmit(evt){
+    function handleSubmite(evt){
         evt.preventDefault();
-        if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)|| name==="" ||surname==="" ||age==="" || password==="" ||email===""){
-            setError(true);
-            
+     
+        if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)|| email==="" || password==="" || name===""|| surname==="" ||age=== ""  ){
+            setError(true)
         }
         else{
             setSubmite(true);
             setError(false);
         }
+
+        const newEntry={name:name,surname:surname,age:age,email:email,password:password};
+        setAllEntry([...allEntry, newEntry]);
+        console.log(allEntry);
      }
 
-    function errorMessage(){
-      return(
-          <div className="error" style={{
-            display: error ? '' : 'none',
-          }}>
-              <span className="error_mesiges">Խնդրում ենք ճիշտ մուտքագրել բոլոր դաշտերը</span>
+     const successMessage = () => {
+        return (
+          <div
+            className="success"
+            style={{
+              display:submite ? '' : 'none',
+            }}>
+            <span className="error_mesiges">շնորհավորում ենք  դուք  գրայնցվեցիք!!</span>
           </div>
-      )
-    }
-
-
-
-   
-
-
-
-
-    return(
-    <div className="register">
-      <div>
-        <h2>ՍՏԵՂԾԵԼ ՆՈՐ ՀԱՇԻՎ</h2>
-      </div>
- 
+        );
+      };
+  
+  
+     
+  
+      function errorMessage(){
+        return(
+            <div className="error" style={{
+              display: error ? '' : 'none',
+            }}>
+                <span >Խնդրում ենք ճիշտ մուտքագրել բոլոր դաշտերը</span>
+            </div>
+        )
+      }
       
-      <div className="errorMessages">
-        {errorMessage()}
-      </div>
-        
-          <form className="registtration_form">
-              
-              <input value={name} placeholder="Անուն" type="text" onChange={handleName}/>
-              <br/>
-              <input value={surname} placeholder="Ազգանուն" type="text" onChange={handleSurname}/>
-              <br/>
-              <input value={age} placeholder="Տարիք" type="number" onChange={handleAge}/>
-              <br/>
-              <input value={email} placeholder="էլ․հասցե" type="text" onChange={handleEmail}/>
-              <br/>
-              <input value={password} placeholder="Գաղտնաբառ" type="password" onChange={handlePassword}/>
-              <br/>
+  
 
-              <button type="Submite"onClick={handleSubmit} className="registration_btn" >Ստեղծել հաշիվ</button>
-              <span>Արդեն գրանցվե ՞լ ես  <NavLink className='navLink777' to='/login'>Մուտք</NavLink></span> 
-               
 
-          </form>
-        </div>
-    )
-}
 
-export default Register
+
+    
+    return(
+        <div className="register">
+            <form  className="registtration_form">
+            
+                <h2>ՍՏԵՂԾԵԼ ՆՈՐ ՀԱՇԻՎ</h2>
+                <div className="errorMessages">
+                   {errorMessage()}
+                   {successMessage()}
+                </div>
+                <input value={name} onChange={handleName} name="name" placeholder="Անուն" type="text"/>
+                <input value={surname} onChange={handleSurname} name="surname" placeholder="Ազգանուն" type="text"/>
+                <input value={age} onChange={handleAge} name="age" placeholder="Տարիք" type="number"/>
+                <input value={email} onChange={handleEmail} name="email" placeholder="Էլ․ հասցե" type="text"/>
+                <input value={password} onChange={handlePassword} name="password" placeholder="Գաղտնաբառ" type="password"/>
+                <button onClick={handleSubmite} className="registration_btn" type="submit">Ստեղծել հաշիվ </button>
+                <span>Եթե չունեք հաշիվ, կարող եք <NavLink className='navLink777' to='/login'>Մուտք</NavLink></span> 
+                
+                
+            </form>
+           
+           
+             </div>
+             )
+         };
+         export default Register
+
